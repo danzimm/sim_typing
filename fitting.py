53bf0e453560993ee9c4057dbb5d12fd29833cda
Dan Zimmerman <daniel.zimmerman@me.com>
2015-03-23 14:08:30 -0500

    put z into bounds

--- fitting.py
+++ fitting.py
@@ -83,6 +83,7 @@ def fit_and_save(metas, datas):
       t0min = dtmin - 30. + t0off
       t0max = dtmax - 30. + t0off
       m['bounds']['t0'] = (t0min, t0max)        # set t0 bounds
+      m['bounds']['z'] = (meta['REDSHIFT_FINAL'] - 0.01, meta['REDSHIFT_FINAL'] + 0.01)
       if True:
         res, modd = nest_lc(data, m['model'], m['param_names'], bounds=m['bounds'], guess_amplitude_bound=True, nobj=50)
         res.chisq = -2. * res.loglmax
